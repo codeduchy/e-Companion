574b90ca3a91aef40f403bf31b2eb18d738369be
codeduchy <codeduchy@gmail.com>
2023-10-06 12:36:11 +0100

    sidebar's

---
+++ components/navbar.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
+import { Sparkles } from "lucide-react";
+import { Poppins } from "next/font/google";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "./mode-toggle";
+import MobileSidebar from "@/components/mobile-sidebar";
+
+const font = Poppins({
+  weight: "600",
+  subsets: ["latin"],
+});
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full z-50 flex justify-between py-2 px-4 border-b border-primary/10 bg-secondary h-16">
+      <div className="flex items-center">
+        <MobileSidebar />
+        <Link href="/">
+          <h1
+            className={cn(
+              "hidden md:block text-xl md:text-3xl font-bold text-primary",
+              font.className
+            )}
+          >
+            eCompanion
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <Button variant="premium" size="sm">
+          Upgrade
+          <Sparkles className="h-4 w-4 fill-white text-white m-2" />
+        </Button>
+        <ModeToggle />
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
